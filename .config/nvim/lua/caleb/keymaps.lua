adf730cc2598fc464db826d27db9310ffa79263c
Caleb Frost Sanderson <caleb@calebsanderson.com>
2022-09-11 21:20:37 -0400

    adds keybind for Telescope

--- .config/nvim/lua/caleb/keymaps.lua
+++ .config/nvim/lua/caleb/keymaps.lua
@@ -86,6 +86,7 @@ keymap("i", "<F3>", "<C-R>=strftime('%H:%M:%S')<CR>", opts)
 -- Telescope --
 -------------------------------------
 -- keymap("n", "<leader>f", "<cmd>Telescope find_files<cr>", opts)
+keymap("n", "<leader>t", "<cmd>Telescope<cr>", opts)
 keymap("n", "<leader>f", "<cmd>lua require'telescope.builtin'.find_files(require('telescope.themes').get_dropdown({ previewer = false }))<cr>", opts)
 keymap("n", "<c-t>", "<cmd>Telescope live_grep<cr>", opts)
 
